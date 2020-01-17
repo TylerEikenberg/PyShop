66c6a314c13e873521fcd3eb3ecca3484a2f78c0
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-01-17 15:38:50 -0500

    Added html page to display products

--- products/views.py
+++ products/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 
 def index(req):
-    return HttpResponse("Hello World")
+    products = Product.objects.all()
+    return render(req, "index.html", {"products": products})
 
 
 def new(req):
